abad806246e422212def238ead81b9c52962263d
Antoine Vancollie <avancoll@fu-r10-p4.s19.be>
2023-02-06 16:26:16 +0100

    improving ft_atoi for push_swap

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: avancoll <avancoll@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 15:31:08 by avancoll          #+#    #+#             */
-/*   Updated: 2023/02/02 16:28:07 by avancoll         ###   ########.fr       */
+/*   Updated: 2023/02/06 16:25:54 by avancoll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,14 @@ int	ft_atoi(const char *s, t_data **data)
 	if (s[i] == '-' || s[i] == '+')
 		if (s[i++] == '-')
 			sign = -sign;
+	if (!s[i])
+		(*data)->error = 1;
 	while (s[i] == '0')
 		i++;
 	while (s[i] && s[i] >= '0' && s[i] <= '9')
 		res = res * 10 + s[i++] - '0';
-	if (s[i] && ((res > 2147483647 && sign == 1)
-			|| (res > 2147483648 && sign == -1) || (s[i] < '0' || s[i] > '9')))
+	if ((s[i] && (s[i] < '0' || s[i] > '9')) || (res > 2147483647 && sign == 1)
+			|| (res > 2147483648 && sign == -1))
 		(*data)->error = 1;
 	return (res * sign);
 }
